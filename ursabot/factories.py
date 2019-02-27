f3cad481f15de9ced0b95834c91c9fd76d6ba69c
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-02-27 14:23:13 +0100

    add factories

---
+++ ursabot/factories.py
@@ -0,0 +1,17 @@
+from buildbot.plugins import util
+
+from .steps import checkout, ls, mkdir, cmake, compile, test, echo
+
+
+factory = util.BuildFactory()
+factory.addStep(checkout)
+factory.addStep(ls)
+factory.addStep(mkdir)
+factory.addStep(cmake)
+factory.addStep(compile)
+factory.addStep(test)
+
+echo_factory = util.BuildFactory()
+echo_factory.addStep(checkout)
+echo_factory.addStep(ls)
+echo_factory.addStep(echo)
