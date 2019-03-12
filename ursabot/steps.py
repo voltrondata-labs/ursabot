cf6ccbca833f211c828992fa1dc3428c3bcacbc3
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-03-12 11:16:44 +0100

    don't pass all of the cmake flags

--- ursabot/steps.py
+++ ursabot/steps.py
@@ -101,141 +101,139 @@ checkout = steps.Git(
     submodules=True
 )
 
-# -DCMAKE_INSTALL_PREFIX=build
-# -DCMAKE_INSTALL_LIBDIR=lib
-# -DARROW_INSTALL_NAME_RPATH=ON
-# -DCMAKE_CXX_FLAGS
-
 # explicitly define build definitions, exported via cmake -LAH
 definitions = {
-    # Build type
+    # CMake flags
     'CMAKE_BUILD_TYPE': 'debug',
+    # 'CMAKE_INSTALL_PREFIX': None,
+    # 'CMAKE_INSTALL_LIBDIR': None,
+    # 'CMAKE_CXX_FLAGS': None,
     # Build Arrow with Altivec
-    'ARROW_ALTIVEC': 'ON',
+    # 'ARROW_ALTIVEC': 'ON',
     # Rely on boost shared libraries where relevant
-    'ARROW_BOOST_USE_SHARED': 'ON',
+    # 'ARROW_BOOST_USE_SHARED': 'ON',
     # Use vendored Boost instead of existing Boost.
     # Note that this requires linking Boost statically.
-    'ARROW_BOOST_VENDORED': 'OFF',
+    # 'ARROW_BOOST_VENDORED': 'OFF',
     # Build the Arrow micro benchmarks
     'ARROW_BUILD_BENCHMARKS': 'OFF',
     # Build the Arrow examples
     'ARROW_BUILD_EXAMPLES': 'OFF',
     # Build shared libraries
-    'ARROW_BUILD_SHARED': 'ON',
+    # 'ARROW_BUILD_SHARED': 'ON',
     # Build static libraries
-    'ARROW_BUILD_STATIC': 'ON',
+    # 'ARROW_BUILD_STATIC': 'ON',
     # Build the Arrow googletest unit tests
     'ARROW_BUILD_TESTS': 'ON',
     # Build Arrow commandline utilities
-    'ARROW_BUILD_UTILITIES': 'ON',
+    # 'ARROW_BUILD_UTILITIES': 'ON',
     # Build the Arrow Compute Modules
-    'ARROW_COMPUTE': 'ON',
+    # 'ARROW_COMPUTE': 'ON',
     # Build the Arrow CUDA extensions (requires CUDA toolkit)
-    'ARROW_CUDA': 'OFF',
+    # 'ARROW_CUDA': 'OFF',
     # Compiler flags to append when compiling Arrow
-    'ARROW_CXXFLAGS': '',
+    # 'ARROW_CXXFLAGS': '',
     # Compile with extra error context (line numbers, code)
     'ARROW_EXTRA_ERROR_CONTEXT': 'OFF',
     # Build the Arrow Flight RPC System (requires GRPC, Protocol Buffers)
-    'ARROW_FLIGHT': 'OFF',
+    # 'ARROW_FLIGHT': 'OFF',
     # Build Arrow Fuzzing executables
-    'ARROW_FUZZING': 'OFF',
+    # 'ARROW_FUZZING': 'OFF',
     # Build the Gandiva libraries
-    'ARROW_GANDIVA': 'OFF',
+    # 'ARROW_GANDIVA': 'OFF',
     # Build the Gandiva JNI wrappers
-    'ARROW_GANDIVA_JAVA': 'OFF',
+    # 'ARROW_GANDIVA_JAVA': 'OFF',
     # Compiler flags to append when pre-compiling Gandiva operations
-    'ARROW_GANDIVA_PC_CXX_FLAGS': '',
+    # 'ARROW_GANDIVA_PC_CXX_FLAGS': '',
     # Include -static-libstdc++ -static-libgcc when linking with Gandiva
     # static libraries
-    'ARROW_GANDIVA_STATIC_LIBSTDCPP': 'OFF',
+    # 'ARROW_GANDIVA_STATIC_LIBSTDCPP': 'OFF',
     # Build with C++ code coverage enabled
-    'ARROW_GENERATE_COVERAGE': 'OFF',
+    # 'ARROW_GENERATE_COVERAGE': 'OFF',
     # Rely on GFlags shared libraries where relevant
-    'ARROW_GFLAGS_USE_SHARED': 'ON',
+    # 'ARROW_GFLAGS_USE_SHARED': 'ON',
     # Pass -ggdb flag to debug builds
-    'ARROW_GGDB_DEBUG': 'ON',
+    # 'ARROW_GGDB_DEBUG': 'ON',
     # Build the Arrow HDFS bridge
-    'ARROW_HDFS': 'ON',
+    # 'ARROW_HDFS': 'ON',
     # Build the HiveServer2 client and Arrow adapter
-    'ARROW_HIVESERVER2': 'OFF',
+    # 'ARROW_HIVESERVER2': 'OFF',
     # Build Arrow libraries with install_name set to @rpath
-    'ARROW_INSTALL_NAME_RPATH': 'ON',
+    # 'ARROW_INSTALL_NAME_RPATH': 'ON',
     # Build the Arrow IPC extensions
-    'ARROW_IPC': 'ON',
+    # 'ARROW_IPC': 'ON',
     # Build the Arrow jemalloc-based allocator
-    'ARROW_JEMALLOC': 'ON',
+    # 'ARROW_JEMALLOC': 'ON',
     # Exclude deprecated APIs from build
-    'ARROW_NO_DEPRECATED_API': 'OFF',
+    # 'ARROW_NO_DEPRECATED_API': 'OFF',
     # Only define the lint and check-format targets
-    'ARROW_ONLY_LINT': 'OFF',
+    # 'ARROW_ONLY_LINT': 'OFF',
     # If enabled install ONLY targets that have already been built.
     # Please be advised that if this is enabled 'install' will fail silently
     # on components that have not been built.
-    'ARROW_OPTIONAL_INSTALL': 'OFF',
+    # 'ARROW_OPTIONAL_INSTALL': 'OFF',
     # Build the Arrow ORC adapter
-    'ARROW_ORC': 'OFF',
+    # 'ARROW_ORC': 'OFF',
     # Build the Parquet libraries
-    'ARROW_PARQUET': 'OFF',
+    # 'ARROW_PARQUET': 'OFF',
     # Build the plasma object store along with Arrow
-    'ARROW_PLASMA': 'OFF',
+    # 'ARROW_PLASMA': 'OFF',
     # Build the plasma object store java client
-    'ARROW_PLASMA_JAVA_CLIENT': 'OFF',
+    # 'ARROW_PLASMA_JAVA_CLIENT': 'OFF',
     # Rely on Protocol Buffers shared libraries where relevant
-    'ARROW_PROTOBUF_USE_SHARED': 'OFF',
+    # 'ARROW_PROTOBUF_USE_SHARED': 'OFF',
     # Build the Arrow CPython extensions
-    'ARROW_PYTHON': 'OFF',
+    # 'ARROW_PYTHON': 'OFF',
     # How to link the re2 library. static|shared
-    'ARROW_RE2_LINKAGE': 'static',
+    # 'ARROW_RE2_LINKAGE': 'static',
     # Build Arrow libraries with RATH set to $ORIGIN
-    'ARROW_RPATH_ORIGIN': 'OFF',
+    # 'ARROW_RPATH_ORIGIN': 'OFF',
     # Build Arrow with TensorFlow support enabled
-    'ARROW_TENSORFLOW': 'OFF',
+    # 'ARROW_TENSORFLOW': 'OFF',
     # Linkage of Arrow libraries with unit tests executables. static|shared
-    'ARROW_TEST_LINKAGE': 'shared',
+    # 'ARROW_TEST_LINKAGE': 'shared',
     # Run the test suite using valgrind --tool=memcheck
-    'ARROW_TEST_MEMCHECK': 'OFF',
+    # 'ARROW_TEST_MEMCHECK': 'OFF',
     # Enable Address Sanitizer checks
-    'ARROW_USE_ASAN': 'OFF',
+    # 'ARROW_USE_ASAN': 'OFF',
     # Use ccache when compiling (if available)
-    'ARROW_USE_CCACHE': 'ON',
+    # 'ARROW_USE_CCACHE': 'ON',
     # Build libraries with glog support for pluggable logging
-    'ARROW_USE_GLOG': 'ON',
+    # 'ARROW_USE_GLOG': 'ON',
     # Use ld.gold for linking on Linux (if available)
-    'ARROW_USE_LD_GOLD': 'OFF',
+    # 'ARROW_USE_LD_GOLD': 'OFF',
     # Build with SIMD optimizations
-    'ARROW_USE_SIMD': 'ON',
+    # 'ARROW_USE_SIMD': 'ON',
     # Enable Thread Sanitizer checks
-    'ARROW_USE_TSAN': 'OFF',
+    # 'ARROW_USE_TSAN': 'OFF',
     # If off, 'quiet' flags will be passed to linting tools
-    'ARROW_VERBOSE_LINT': 'OFF',
+    # 'ARROW_VERBOSE_LINT': 'OFF',
     # If off, output from ExternalProjects will be logged to files rather
     # than shown
     'ARROW_VERBOSE_THIRDPARTY_BUILD': 'ON',
     # Build with backtrace support
     'ARROW_WITH_BACKTRACE': 'ON',
     # Build with Brotli compression
-    'ARROW_WITH_BROTLI': 'ON',
+    # 'ARROW_WITH_BROTLI': 'ON',
     # Build with BZ2 compression
-    'ARROW_WITH_BZ2': 'OFF',
+    # 'ARROW_WITH_BZ2': 'OFF',
     # Build with lz4 compression
-    'ARROW_WITH_LZ4': 'ON',
+    # 'ARROW_WITH_LZ4': 'ON',
     # Build with Snappy compression
-    'ARROW_WITH_SNAPPY': 'ON',
+    # 'ARROW_WITH_SNAPPY': 'ON',
     # Build with zlib compression
-    'ARROW_WITH_ZLIB': 'ON',
+    # 'ARROW_WITH_ZLIB': 'ON',
     # Build with zstd compression, turned off until
     # https://issues.apache.org/jira/browse/ARROW-4831 is resolved
     'ARROW_WITH_ZSTD': 'OFF',
     # Build the Parquet examples. Requires static libraries to be built.
-    'PARQUET_BUILD_EXAMPLES': 'OFF',
+    # 'PARQUET_BUILD_EXAMPLES': 'OFF',
     # Build the Parquet executable CLI tools.
     # Requires static libraries to be built.
-    'PARQUET_BUILD_EXECUTABLES': 'OFF',
+    # 'PARQUET_BUILD_EXECUTABLES': 'OFF',
     # Depend only on Thirdparty headers to build libparquet.
     # Always OFF if building binaries
-    'PARQUET_MINIMAL_DEPENDENCY': 'OFF'
+    # 'PARQUET_MINIMAL_DEPENDENCY': 'OFF'
 }
 definitions = {k: util.Property(k, default=v) for k, v in definitions.items()}
 
@@ -257,11 +255,11 @@ cmake = CMake(
 )
 
 # TODO(kszucs): use property
-compile = BashCommand(
+compile = ShellCommand(
     command=['ninja'],
     workdir='build'
 )
-test = BashCommand(
+test = ShellCommand(
     command=['ninja', 'test'],
     workdir='build'
 )
@@ -269,7 +267,7 @@ test = BashCommand(
 aranlib = steps.SetPropertiesFromEnv(variables=['AR', 'RANLIB'])
 
 env_old = steps.ShellCommand(command=['env'])
-env = BashCommand(command=['env'])
+env = ShellCommand(command=['env'])
 
 ls = steps.ShellCommand(command=['ls', '-lah'])
 echo = steps.ShellCommand(command=['echo', 'testing...'])
