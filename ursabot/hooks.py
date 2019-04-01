e04f57ce8aa30c0a7a47966dbde229403ac92012
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-04-01 16:41:05 +0200

    respond on issue comment

--- ursabot/hooks.py
+++ ursabot/hooks.py
@@ -1,10 +1,32 @@
 # from twisted.python import log
+from twisted.internet import defer
 
 from buildbot.www.hooks.github import GitHubEventHandler
+from buildbot.util.httpclientservice import HTTPClientService
 
 
+# rename it to listener
 class GithubHook(GitHubEventHandler):
 
-    def handle_blah(self, payload):
-        # Do some magic here
-        return [], 'git'
+    def _get_github_client(self):
+        headers = {'User-Agent': 'Buildbot'}
+        if self._token:
+            headers['Authorization'] = 'token ' + self._token
+
+        return HTTPClientService.getService(
+            self.master, self.github_api_endpoint, headers=headers,
+            debug=self.debug, verify=self.verify)
+
+    @defer.inlineCallbacks
+    def handle_issue_comment(self, payload, event):
+        body = payload['comment']['body']
+
+        if body.startswith('@ursabot '):
+            response = 'Good command!'
+        else:
+            response = 'Wrong command, start with @ursabot!'
+
+        client = self._get_github_client()
+        url = payload['issue']['comments_url']
+        data = {'body': response}
+        yield client.post(url, data=data)
