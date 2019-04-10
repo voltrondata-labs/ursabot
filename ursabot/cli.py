8d18010ac09b7e450a0d46ba63f20b49cf1675a6
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-04-10 16:04:41 +0200

    docker client argument to build images on specific hosts

--- ursabot/cli.py
+++ ursabot/cli.py
@@ -7,31 +7,38 @@ import logging
 from .docker import arrow_images
 
 
+logging.basicConfig()
 logger = logging.getLogger(__name__)
 
 
 @click.group()
-@click.option('--quiet/--verbose', '-q', default=False, is_flag=True)
-def ursabot(quiet):
-    if quiet:
-        logger.setLevel(logging.ERROR)
-    else:
+@click.option('--verbose/--quiet', '-v', default=False, is_flag=True)
+@click.pass_context
+def ursabot(ctx, verbose):
+    ctx.ensure_object(dict)
+    ctx.obj['verbose'] = verbose
+
+    if verbose:
         logger.setLevel(logging.INFO)
 
 
 @ursabot.group()
-def docker():
-    pass
+@click.pass_context
+def docker(ctx):
+    if ctx.obj['verbose']:
+        logging.getLogger('dockermap').setLevel(logging.INFO)
 
 
 @docker.command()
+@click.option('--client', '-c', default=None,
+              help='Docker client url in form: tcp://127.0.0.1:2375')
 @click.option('--push/--no-push', '-p', default=False,
               help='Push the built images')
 @click.option('--architecture', '-a', default=None,
               help='Build docker images for this specifig architecture')
 @click.option('--filter', '-f', default=None,
               help='Filter images by name')
-def build(push, architecture, filter):
+def build(client, push, architecture, filter):
     if architecture is not None:
         imgs = [img for img in arrow_images if img.arch == architecture]
     else:
@@ -42,12 +49,12 @@ def build(push, architecture, filter):
 
     for img in imgs:
         click.echo(f'Building {img.fqn}')
-        img.build()
+        img.build(client=client)
 
     if push:
         for img in imgs:
             click.echo(f'Pushing {img.fqn}...')
-            img.push()
+            img.push(client=client)
 
     # Build eagerly for now
     # with ProgressBar():
