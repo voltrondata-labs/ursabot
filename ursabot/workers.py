b444a3eb972a644aa5c7fcc95f6056ca441e3f47
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-04-28 22:27:07 +0200

    cleanup outdated master conf

--- ursabot/workers.py
+++ ursabot/workers.py
@@ -1,6 +1,13 @@
 from buildbot.plugins import worker
 
 
+# TODO: support specifying parameters to improve isolation, like:
+#   cpu_shares, isolation(cgroups), mem_limit and runtime (which will be
+#   required for nvidia builds)
+# https://docker-py.readthedocs.io/en/stable/api.html
+# docker.api.container.ContainerApiMixin.create_host_config
+
+
 class WorkerMixin:
 
     def __init__(self, *args, arch, **kwargs):
