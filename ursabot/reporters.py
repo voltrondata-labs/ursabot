769e34ee8322120eed411bbe189b2bce85b80ac2
François Saint-Jacques <fsaintjacques@gmail.com>
2019-05-01 11:18:36 -0400

    Implement GitHubReviewPush
    - Refactor configuration such that StatusPush are only enabled for
      ursabot builders.
    - Added builder Mixin similar to schedulers.
    - Implement GitHubReviewPush that doesn't require GitHubApp.

--- ursabot/reporters.py
+++ ursabot/reporters.py
@@ -10,6 +10,12 @@ _template = u'''\
 <p><b> -- The Buildbot</b></p>
 '''
 
+class BuilderReporterMixin:
+
+    def __init__(self, *args, builders, **kwargs):
+        builder_names = [b.name for b in builders]
+        super().__init__(*args, builders=builder_names, **kwargs)
+
 
 class ZulipMailNotifier(reporters.MailNotifier):
 
@@ -23,3 +29,49 @@ class ZulipMailNotifier(reporters.MailNotifier):
         super().__init__(fromaddr=fromaddr, extraRecipients=[zulipaddr],
                          messageFormatter=formatter,
                          sendToInterestedUsers=False)
+
+
+class GitHubStatusPush(BuilderReporterMixin, reporters.GitHubStatusPush):
+    pass
+
+
+class GitHubCommentPush(BuilderReporterMixin, reporters.GitHubCommentPush):
+    pass
+
+
+class GitHubReviewPush(GitHubCommentPush):
+    name = "GitHubReviewPush"
+
+    def createStatus(self,
+                     repo_user, repo_name, sha, state, target_url=None,
+                     context=None, issue=None, description=None):
+        """
+        :param repo_user: GitHub user or organization
+        :param repo_name: Name of the repository
+        :param issue: Pull request number
+        :param state: one of the following 'pending', 'success', 'error'
+                      or 'failure'.
+        :param description: Short description of the status.
+        :return: A deferred with the result from GitHub.
+        This code comes from txgithub by @tomprince.
+        txgithub is based on twisted's webclient agent, which is much less reliable and featureful
+        as txrequest (support for proxy, connection pool, keep alive, retry, etc)
+        """
+
+        # Convert state into the expected review status.
+        review_status = {
+            'success': 'APPROVE',
+            'pending': 'PENDING',
+            'error': 'REQUEST_CHANGES',
+            'failure': 'REQUEST_CHANGES',
+        }[state]
+
+        payload = {
+            'commit_id': sha,
+            'event': review_status,
+            'body': description
+        }
+
+        return self._http.post(
+            '/'.join(['/repos', repo_user, repo_name, 'pulls', issue, 'reviews']),
+            json=payload)
