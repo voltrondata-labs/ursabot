8c77a3bce0ea597cf20a3804bc5f1ca6e307c400
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-05-01 17:38:38 +0200

    use bash for conda images

--- ursabot/docker.py
+++ ursabot/docker.py
@@ -343,9 +343,9 @@ for arch in ['amd64']:
         ADD(docker_assets / 'conda-linux.txt'),
         ADD(docker_assets / 'conda-cpp.txt'),
         RUN(conda(files=['conda-linux.txt', 'conda-cpp.txt'])),
-        # run conda init
-        SHELL(login_shell),
-        ENTRYPOINT(login_shell)
+        # run conda init...
+        SHELL(['/bin/bash', '-i', '-c']),
+        ENTRYPOINT(['/bin/bash', '-i', '-c']),
     ]
     cpp = DockerImage('cpp', base=base, arch=arch, os=os, variant='conda',
                       steps=steps)
