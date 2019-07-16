d9808d0381dd256a5cdf3fc7330368b9a82f213a
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-07-17 01:24:34 +0200

    JavaScript builder and support for volumes (#131)
    * js builder
    
    * generate volumes

--- ursabot/utils.py
+++ ursabot/utils.py
@@ -39,11 +39,7 @@ def read_dependency_list(path):
 
 
 def slugify(s):
-    '''Slugify CamelCase name'''
-    s = re.sub(r'[\W\-]+', '-', s)
-    s = re.sub(r'([A-Z])', lambda m: '-' + m.group(1).lower(), s)
-    s = s.strip('-')
-    return s
+    return re.sub(r'[\W_]+', '-', s)
 
 
 class Filter:
