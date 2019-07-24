dfdc33ba2c6843ddfdc81e3b353a46bf830f76d7
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-07-24 16:39:41 +0200

    Add the list of affected files to the changes created by pull request triggers (#139)
    * reimplement handle pull request
    
    * flake8
    
    * ensure that the queried files are a list

--- ursabot/tests/test_hooks.py
+++ ursabot/tests/test_hooks.py
@@ -10,6 +10,7 @@
 
 import json
 from pathlib import Path
+
 from twisted.trial import unittest
 
 from buildbot.plugins import util
@@ -19,7 +20,7 @@ from buildbot.www.change_hook import ChangeHookResource
 from buildbot.test.unit.test_www_hooks_github import _prepare_request
 
 from ursabot.utils import ensure_deferred
-from ursabot.hooks import UrsabotHook
+from ursabot.hooks import GithubHook, UrsabotHook
 from ursabot.commands import CommandError, ursabot as ursabot_command
 from ursabot.tests.mocks import GithubClientService
 
@@ -39,6 +40,7 @@ def _prepare_github_change_hook(testcase, **params):
 class ChangeHookTestCase(unittest.TestCase, TestReactorMixin):
 
     klass = None
+    user_agent = 'Buildbot'
 
     @ensure_deferred
     async def setUp(self):
@@ -60,7 +62,7 @@ class ChangeHookTestCase(unittest.TestCase, TestReactorMixin):
             self.master,
             self,
             'https://api.github.com',
-            headers={'User-Agent': 'Ursabot'},
+            headers={'User-Agent': self.user_agent},
             tokens=['test-token'],
             debug=False,
             verify=False
@@ -84,6 +86,65 @@ class ChangeHookTestCase(unittest.TestCase, TestReactorMixin):
         with path.open('r') as fp:
             return json.load(fp)
 
+    @property
+    def changes_added(self):
+        return self.hook.master.data.updates.changesAdded
+
+
+class TestGithubHook(ChangeHookTestCase):
+    # License note:
+    #    Copied from the original buildbot implementation with
+    #    minor changes and additions.
+
+    klass = GithubHook
+
+    @ensure_deferred
+    async def test_pull_request(self):
+        payload = self.load_fixture('event-pull-request-opened')
+
+        # handle_pull_request fetches the commit message
+        request_json = self.load_fixture('pull-request-26-commit')
+        commit = '2705da2b616b98fa6010a25813c5a7a27456f71d'
+        self.http.expect('get', f'/repos/ursa-labs/ursabot/commits/{commit}',
+                         content_json=request_json)
+
+        # handle_pull_request fetches the affected files
+        request_json = self.load_fixture('pull-request-26-files')
+        self.http.expect('get', f'/repos/ursa-labs/ursabot/pulls/26/files',
+                         content_json=request_json)
+
+        expected_files = [f['filename'] for f in request_json]
+
+        # trigger the event
+        await self.trigger('pull_request', payload=payload)
+
+        assert len(self.changes_added) == 1
+        change = self.changes_added[0]
+
+        assert change['repository'] == 'https://github.com/ursa-labs/ursabot'
+        assert change['author'] == 'kszucs'
+        assert change['branch'] == 'refs/pull/26/merge'
+        assert change['revlink'] == (
+            'https://github.com/ursa-labs/ursabot/pull/26'
+        )
+        assert change['properties']['basename'] == 'master'
+        assert change['files'] == expected_files
+
+    @ensure_deferred
+    async def test_pull_request_with_skip_message(self):
+        payload = self.load_fixture('event-pull-request-opened')
+
+        # handle_pull_request fetches the commit message, it won't fetch the
+        # affected files if the commit contains a skip pattern
+        request_json = self.load_fixture('pull-request-26-commit')
+        request_json['commit']['message'] = 'commit message [skip ci]'
+        commit = '2705da2b616b98fa6010a25813c5a7a27456f71d'
+        self.http.expect('get', f'/repos/ursa-labs/ursabot/commits/{commit}',
+                         content_json=request_json)
+
+        await self.trigger('pull_request', payload=payload)
+        assert len(self.changes_added) == 0
+
 
 # XXX: hack for testing ursabot hook with comment reactions insted, patching is
 # messed up in the original test suite
@@ -94,30 +155,35 @@ class NoReactionsUrsabotHook(UrsabotHook):
 class TestUrsabotHook(ChangeHookTestCase):
 
     klass = NoReactionsUrsabotHook
+    user_agent = 'Ursabot'
 
     @ensure_deferred
     async def test_ping(self):
         await self.trigger('ping', {})
-        assert len(self.hook.master.data.updates.changesAdded) == 0
+        assert len(self.changes_added) == 0
 
     @ensure_deferred
     async def test_issue_comment_not_mentioning_ursabot(self):
-        payload = self.load_fixture('issue-comment-not-mentioning-ursabot')
+        payload = self.load_fixture(
+            'event-issue-comment-not-mentioning-ursabot'
+        )
         await self.trigger('issue_comment', payload=payload)
-        assert len(self.hook.master.data.updates.changesAdded) == 0
+        assert len(self.changes_added) == 0
 
     @ensure_deferred
     async def test_issue_comment_by_ursabot(self):
         # don't respond to itself, it prevents recursive comment storms!
-        payload = self.load_fixture('issue-comment-by-ursabot')
+        payload = self.load_fixture('event-issue-comment-by-ursabot')
         await self.trigger('issue_comment', payload=payload)
-        assert len(self.hook.master.data.updates.changesAdded) == 0
+        assert len(self.changes_added) == 0
 
     @ensure_deferred
     async def test_issue_comment_by_non_authorized_user(self):
-        payload = self.load_fixture('issue-comment-by-non-authorized-user')
+        payload = self.load_fixture(
+            'event-issue-comment-by-non-authorized-user'
+        )
         await self.trigger('issue_comment', payload=payload)
-        assert len(self.hook.master.data.updates.changesAdded) == 0
+        assert len(self.changes_added) == 0
 
     @ensure_deferred
     async def test_issue_comment_with_empty_command_reponds_with_usage(self):
@@ -132,9 +198,9 @@ class TestUrsabotHook(ChangeHookTestCase):
         self.http.expect('post', '/repos/ursa-labs/ursabot/issues/26/comments',
                          json=request_json, content_json=response_json)
 
-        payload = self.load_fixture('issue-comment-with-empty-command')
+        payload = self.load_fixture('event-issue-comment-with-empty-command')
         await self.trigger('issue_comment', payload=payload)
-        assert len(self.hook.master.data.updates.changesAdded) == 0
+        assert len(self.changes_added) == 0
 
     @ensure_deferred
     async def test_issue_comment_without_pull_request(self):
@@ -146,21 +212,28 @@ class TestUrsabotHook(ChangeHookTestCase):
         self.http.expect('post', '/repos/ursa-labs/ursabot/issues/19/comments',
                          json=request_json, content_json=response_json)
 
-        payload = self.load_fixture('issue-comment-without-pull-request')
+        payload = self.load_fixture('event-issue-comment-without-pull-request')
         await self.trigger('issue_comment', payload=payload)
-        assert len(self.hook.master.data.updates.changesAdded) == 0
+        assert len(self.changes_added) == 0
 
     @ensure_deferred
-    async def check_issue_comment_with_command(self, command,
-                                               expected_props=None):
+    async def check_issue_comment_with_command(
+        self, command, expected_props=None, commit_message=None
+    ):
         # handle_issue_comment queries the pull request
         request_json = self.load_fixture('pull-request-26')
         self.http.expect('get', '/repos/ursa-labs/ursabot/pulls/26',
                          content_json=request_json)
         # trigger handle_pull_request which fetches the commit
         request_json = self.load_fixture('pull-request-26-commit')
+        if commit_message is not None:
+            request_json['commit']['message'] = commit_message
         commit = '2705da2b616b98fa6010a25813c5a7a27456f71d'
         self.http.expect('get', f'/repos/ursa-labs/ursabot/commits/{commit}',
+                         content_json=request_json)
+        # trigger handle_pull_request which fetches the affected files
+        request_json = self.load_fixture('pull-request-26-files')
+        self.http.expect('get', f'/repos/ursa-labs/ursabot/pulls/26/files',
                          content_json=request_json)
 
         # then responds to the comment
@@ -170,14 +243,14 @@ class TestUrsabotHook(ChangeHookTestCase):
         self.http.expect('post', request_url, json=request_json,
                          content_json=response_json)
 
-        payload = self.load_fixture('issue-comment-build-command')
+        payload = self.load_fixture('event-issue-comment-build-command')
         payload['comment']['body'] = f'@ursabot {command}'
         await self.trigger('issue_comment', payload=payload)
 
         expected_title = 'Unittests for GithubHook'
 
-        assert len(self.hook.master.data.updates.changesAdded) == 1
-        for change in self.hook.master.data.updates.changesAdded:
+        assert len(self.changes_added) == 1
+        for change in self.changes_added:
             assert change['category'] == 'comment'
             assert change['properties']['event'] == 'issue_comment'
             assert change['properties']['github.title'] == expected_title
@@ -189,6 +262,16 @@ class TestUrsabotHook(ChangeHookTestCase):
         await self.check_issue_comment_with_command(
             command='build',
             expected_props={'command': 'build'}
+        )
+
+    @ensure_deferred
+    async def test_issue_comment_build_command_with_skip_pattern(self):
+        # the build should be triggered even if the commit message contains
+        # skip ci patterns
+        await self.check_issue_comment_with_command(
+            command='build',
+            expected_props={'command': 'build'},
+            commit_message='commit message [skip ci]'
         )
 
     @ensure_deferred
