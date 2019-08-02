657f084306889ff34510827d48a6160eb29f8d41
Krisztián Szűcs <szucs.krisztian@gmail.com>
2019-08-02 16:47:51 +0200

    duplicate entrypoints are not allowed by buildbot's plugin system

--- setup.py
+++ setup.py
@@ -12,10 +12,6 @@ if sys.version_info < (3, 6):
     sys.exit('Python < 3.6 is not supported due to missing asyncio support')
 
 
-def plugins(module, symbols):
-    return ['{1} = {0}:{1}'.format(module, symbol) for symbol in symbols]
-
-
 # TODO(kszucs): add package data, change maintainer
 setup(
     name='ursabot',
@@ -33,7 +29,6 @@ setup(
         'buildbot-www',
         'buildbot',
         'click',
-        'codenamize',
         'docker-map',
         'docker',
         'python-dotenv',
@@ -48,109 +43,7 @@ setup(
     entry_points={
         'console_scripts': [
             'ursabot = ursabot.cli:ursabot'
-        ],
-        'buildbot.changes': plugins(module='ursabot.changes', symbols=[
-            'ChangeFilter',
-            'GitPoller',
-            'GitHubPullrequestPoller'
-        ]),
-        'buildbot.steps': plugins(module='ursabot.steps', symbols=[
-            'Mkdir',
-            'GitHub',
-            'Cargo',
-            'CMake',
-            'CTest',
-            'Env',
-            'Go',
-            'Make',
-            'Maven',
-            'Ninja',
-            'Npm',
-            'Pip',
-            'PyTest',
-            'R',
-            'ResultLogMixin',
-            'SetPropertiesFromEnv',
-            'SetPropertyFromCommand',
-            'SetupPy',
-            'ShellCommand',
-        ]),
-        'buildbot.schedulers': plugins(module='ursabot.schedulers', symbols=[
-            'ForceScheduler',
-            'TryScheduler',
-            'AnyBranchScheduler',
-            'SingleBranchScheduler',
-        ]),
-        'buildbot.worker': plugins(module='ursabot.worker', symbols=[
-            'DockerLatentWorker',
-        ]),
-        'buildbot.webhooks': plugins(module='ursabot.hooks', symbols=[
-            'GithubHoook',
-            'UrsabotHook',
-        ]),
-        'buildbot.secrets': plugins(module='ursabot.secrets', symbols=[
-            'SecretInPass',
-        ]),
-        'buildbot.reporters': (
-            plugins(module='ursabot.reporters', symbols=[
-                'HttpStatusPush',
-                'GitHubReporter',
-                'GitHubStatusPush',
-                'GitHubReviewPush',
-                'GitHubCommentPush',
-                'ZulipStatusPush',
-            ]) +
-            plugins(module='ursabot.formatters', symbols=[
-                'Formatter',
-                'MarkdownFormatter',
-            ])
-        ),
-        'buildbot.util': (
-            plugins(module='ursabot.configs', symbols=[
-                'Config',
-                'MasterConfig',
-                'ProjectConfig',
-                'InMemoryLoader',
-                'FileLoader',
-                'collect_global_errors',
-            ]) +
-            plugins(module='ursabot.builders', symbols=[
-                'BuildFactory',
-                'Builder',
-                'DockerBuilder',
-            ]) +
-            plugins(module='ursabot.docker', symbols=[
-                'DockerFile',
-                'DockerImage',
-                'ImageCollection',
-                'worker_image_for',
-                'worker_images_for',
-                'ADD',
-                'COPY',
-                'RUN',
-                'ENV',
-                'WORKDIR',
-                'USER',
-                'CMD',
-                'SHELL',
-                'ENTRYPOINT',
-                'symlink',
-                'apt',
-                'apk',
-                'pip',
-                'conda',
-            ]) +
-            plugins(module='ursabot.utils', symbols=[
-                'GithubClientService',
-                'Collection',
-                'Filter',
-                'ensure_deferred',
-                'read_dependency_list',
-                'startswith',
-                'any_of',
-                'has',
-            ])
-        )
+        ]
     },
     zip_safe=False,
 )
