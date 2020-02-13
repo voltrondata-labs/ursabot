0a2efee90b08cebf4af92eb417c11843ccacdee5
Krisztián Szűcs <szucs.krisztian@gmail.com>
2020-02-13 11:51:48 +0100

    trailing whitespace

--- projects/arrow/arrow/crossbow.py
+++ projects/arrow/arrow/crossbow.py
@@ -85,7 +85,7 @@ class CrossbowReport(CrossbowBuilder):
             extract_fn=lambda stdout, stderr: stdout.strip(),
             command=Crossbow(
                 args=[
-                    '--github-token', 
+                    '--github-token',
                     util.Secret('kszucs/github_status_token'),
                     'latest-prefix', crossbow_prefix
                 ]
